2735b26a83f591638a9f020b40f9803b15d22d7c
NB <nb@FaStiCkeR.local>
2026-04-07 14:50:05 +0300

    fix

--- Wordle/frontend/src/config.js
+++ Wordle/frontend/src/config.js
@@ -5,7 +5,7 @@
 /**
  * Расположение API сервиса
  */
-export const API_URL = 'https://localhost:8000';
+export const API_URL = 'http://localhost:8000';
 
 /**
  * Эндпоинт для получения случайного слова
